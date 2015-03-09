57208d3c784f4b572e0d11a7ac32ee4a1cbab9c4
Frédéric DUBOUCHET <frederic.dubouchet@idiap.ch>
2015-03-09 09:35:41 +0100

    solved the bug in simple.cpp

--- Examples/Simple/simple.cpp
+++ Examples/Simple/simple.cpp
@@ -13,30 +13,36 @@ std::string kernel_source = "\n"\
 "      __global read_only float* in1,\n"\
 "      __global read_only float* in2,\n"\
 "      __global write_only float* out)\n"\
-"{"\
+"{\n"\
 "   const uint pos = get_global_id(0);\n"\
 "   out[pos] = in1[pos] * in2[pos];\n"\
 "}\n"\
 "\n";
 
+const unsigned int platform_id = 0;
+const unsigned int device_id = 0;
+
 int main(int ac, char** av) {
-	unsigned int vector_size = 1024;
+	size_t vector_size = 1024 * 256;
 	if (ac > 1)
 		vector_size = atoi(av[1]);
 	try {
 		// get the device (here the GPU)
 		std::vector<cl::Platform> platforms;
-		std::vector<cl::Device> devices_;
 		cl::Platform::get(&platforms);
-		platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices_);
+		std::vector<cl::Device> devices_;
+		platforms[platform_id].getDevices(CL_DEVICE_TYPE_ALL, &devices_);
+		std::cout << "using device : ";
+		std::cout << devices_[device_id].getInfo<CL_DEVICE_NAME>();
+		std::cout << std::endl;
 		cl_context_properties properties[] = {
 			CL_CONTEXT_PLATFORM,
-			(cl_context_properties)(platforms[0])(),
+			(cl_context_properties)(platforms[platform_id])(),
 			0
 		};
-		cl::Context context_ = cl::Context(CL_DEVICE_TYPE_GPU, properties);
+		cl::Context context_ = cl::Context(CL_DEVICE_TYPE_ALL, properties);
 		devices_ = context_.getInfo<CL_CONTEXT_DEVICES>();
-		cl::CommandQueue queue_(context_, devices_[0]);
+		cl::CommandQueue queue_(context_, devices_[device_id], 0 , nullptr);
 		// compile
 		cl::Program::Sources source(
 			1,
@@ -48,6 +54,7 @@ int main(int ac, char** av) {
 		// prepare the buffers
 		std::vector<float> in1(vector_size);
 		std::vector<float> in2(vector_size);
+		std::vector<float> out(vector_size);
 		for (std::vector<float>::iterator ite = in1.begin(); ite != in1.end(); ++ite)
 			(*ite) = (float)random();
 		for (std::vector<float>::iterator ite = in2.begin(); ite != in2.end(); ++ite)
@@ -55,17 +62,17 @@ int main(int ac, char** av) {
 		cl::Buffer buf_in1_ = cl::Buffer(
 			context_,
 			CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			sizeof(float) * vector_size,
-			(void*)&in1);
+			sizeof(cl_float) * in1.size(),
+			(void*)&in1[0]);
 		cl::Buffer buf_in2_ = cl::Buffer(
 			context_,
 			CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
-			sizeof(float) * vector_size,
-			(void*)&in2);
+			sizeof(cl_float) * in2.size(),
+			(void*)&in2[0]);
 		cl::Buffer buf_out_ = cl::Buffer(
 			context_,
 			CL_MEM_WRITE_ONLY,
-			sizeof(float) * vector_size);
+			sizeof(cl_float) * out.size());
 		// set the arguments
 		kernel_.setArg(0, buf_in1_);
 		kernel_.setArg(1, buf_in2_);
@@ -73,7 +80,6 @@ int main(int ac, char** av) {
 		// wait to the command queue to finish before proceeding
 		queue_.finish();
 		// run the kernel
-		std::vector<float> out(vector_size);
 		queue_.enqueueNDRangeKernel(
 			kernel_,
 			cl::NullRange,
